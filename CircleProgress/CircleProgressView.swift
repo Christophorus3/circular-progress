3a98cb79f8ec844fa5122c21e4601fb4b2b5d8f3
christophwottawa <christoph.wottawa@me.com>
2019-07-14 17:12:19 +0200

    first version of circle view animated

---
+++ CircleProgress/CircleProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleProgressView.swift
+//  CircleProgress
+//
+//  Created by Christoph Wottawa on 14.07.19.
+//  Copyright © 2019 Christoph Wottawa. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CircleProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CircleProgress/ViewController.swift
+++ CircleProgress/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let progressLayer = CAShapeLayer()
+    let trackLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.gray.cgColor
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.strokeEnd = 1
+        trackLayer.lineCap = .round
+        
+        view.layer.addSublayer(trackLayer)
+
+        progressLayer.path = circularPath.cgPath
+        progressLayer.strokeColor = UIColor.red.cgColor
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.lineWidth = 10
+        progressLayer.strokeEnd = 0
+        progressLayer.lineCap = .round
+        
+        view.layer.addSublayer(progressLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
 
-
+    @objc private func handleTap() {
+        print("view tapped")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        progressLayer.add(basicAnimation, forKey: "basic")
+    }
 }
 
