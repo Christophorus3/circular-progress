393a330b7e49fdf5c49db087fba8056ce18299c1
christophwottawa <christoph.wottawa@me.com>
2019-07-17 16:27:27 +0200

    refactored layer drawing into UIControll Subclass

--- CircleProgress/CircleProgressView.swift
+++ CircleProgress/CircleProgressView.swift
@@ -8,14 +8,101 @@
 
 import UIKit
 
-@IBDesignable class CircleProgressView: UIView {
+@IBDesignable class CircleProgressView: UIControl {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var progress: Double = 0.0 {
+        didSet {
+            self.percentageLabel.text = "\(Int(progress * 100))%"
+            self.progressLayer.strokeEnd = CGFloat(progress)
+        }
     }
-    */
+    
+    private var progressLayer: CAShapeLayer!
+    private var trackLayer: CAShapeLayer!
+    private var pulseLayer: CAShapeLayer!
+    private var localCenter:CGPoint {
+        return CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+    }
+    private let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .white
+        return label
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+    
+    private func setup() {
+        self.backgroundColor = .backgroundColor
+        
+        setupNotificationObservers()
+        
+        pulseLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: .pulsatingFillColor)
+        self.layer.addSublayer(pulseLayer)
+        animatePulseLayer()
+        
+        trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
+        self.layer.addSublayer(trackLayer)
+        
+        progressLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
+        //rotate the thing to start progress on top (12 o' clock):
+        progressLayer.transform = CATransform3DMakeRotation( -.pi / 2, 0, 0, 1)
+        self.layer.addSublayer(progressLayer)
+        
+        self.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = localCenter
+    }
+    
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    @objc private func handleEnterForeground() {
+        animatePulseLayer()
+    }
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: .pi * 2, clockwise: true)
+        
+        let layer = CAShapeLayer()
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.fillColor = fillColor.cgColor
+        layer.strokeEnd = 1
+        layer.lineWidth = 20
+        layer.lineCap = .round
+        layer.position = localCenter
+        
+        return layer
+    }
 
+    private func animatePulseLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue = 1.5
+        animation.duration = 1
+        animation.autoreverses = true
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        animation.repeatCount = .greatestFiniteMagnitude
+        
+        pulseLayer.add(animation, forKey: "pulse")
+    }
+    
+    //isn't actually in use anymore...
+    fileprivate func animateProgress(to progress: Double) {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        basicAnimation.toValue = progress
+        basicAnimation.duration = 1
+        basicAnimation.fillMode = .forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        progressLayer.add(basicAnimation, forKey: "basic")
+    }
 }
--- CircleProgress/ViewController.swift
+++ CircleProgress/ViewController.swift
@@ -9,106 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let progressLayer = CAShapeLayer()
-    let trackLayer = CAShapeLayer()
-    let pulseLayer = CAShapeLayer()
-    let progress = 0.0
-    let percentageLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Start"
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 32)
-        label.textColor = .white
-        return label
-    }()
+    
+    
+    @IBOutlet weak var prgressView: CircleProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNotificationObservers()
+        //setupNotificationObservers()
         
         view.backgroundColor = .backgroundColor
-        
-        let center = view.center
-        
-        // start angle
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: .pi * 2, clockwise: true)
-        
-        pulseLayer.path = circularPath.cgPath
-        pulseLayer.strokeColor = UIColor.clear.cgColor
-        pulseLayer.fillColor = UIColor.pulsatingFillColor.cgColor
-        pulseLayer.strokeEnd = 1
-        pulseLayer.lineCap = .round
-        pulseLayer.position = center
-        view.layer.addSublayer(pulseLayer)
-        
-        animatePulseLayer()
-        
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.trackStrokeColor.cgColor
-        trackLayer.fillColor = UIColor.backgroundColor.cgColor
-        trackLayer.lineWidth = 20
-        trackLayer.strokeEnd = 1
-        trackLayer.lineCap = .round
-        trackLayer.position = center
-        view.layer.addSublayer(trackLayer)
-
-        progressLayer.path = circularPath.cgPath
-        progressLayer.strokeColor = UIColor.outlineStrokeColor.cgColor
-        progressLayer.fillColor = UIColor.clear.cgColor
-        progressLayer.lineWidth = 20
-        progressLayer.strokeEnd = 0
-        progressLayer.lineCap = .round
-        progressLayer.position = center
-        //rotate the thing to start progress on top (12 o' clock):
-        progressLayer.transform = CATransform3DMakeRotation( -.pi / 2, 0, 0, 1)
-        view.layer.addSublayer(progressLayer)
-        
-        view.addSubview(percentageLabel)
-        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        percentageLabel.center = center
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
-    fileprivate func setupNotificationObservers() {
-        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
-    }
     
-    @objc private func handleEnterForeground() {
-        animatePulseLayer()
-    }
-    
-    fileprivate func animatePulseLayer() {
-        let animation = CABasicAnimation(keyPath: "transform.scale")
-        
-        animation.toValue = 1.5
-        animation.duration = 1
-        animation.autoreverses = true
-        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
-        animation.repeatCount = .greatestFiniteMagnitude
-        
-        pulseLayer.add(animation, forKey: "pulse")
-    }
-
-    //isn't actually in use anymore...
-    fileprivate func animateProgress(to progress: Double) {
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        basicAnimation.toValue = progress
-        basicAnimation.duration = 1
-        basicAnimation.fillMode = .forwards
-        basicAnimation.isRemovedOnCompletion = false
-        
-        progressLayer.add(basicAnimation, forKey: "basic")
+    @IBAction func startTapped(_ sender: CircleProgressView) {
+        handleTap()
     }
     
     @objc private func handleTap() {
         print("view tapped")
         
         downloadFile()
-        
     }
     
     private func downloadFile() {
@@ -140,8 +61,9 @@ extension ViewController: URLSessionDownloadDelegate {
             //this doesn't work as expected...
             //self.animateProgress(to: progress)
             //do this instead:
-            self.percentageLabel.text = "\(Int(progress * 100))%"
-            self.progressLayer.strokeEnd = CGFloat(progress)
+            //self.percentageLabel.text = "\(Int(progress * 100))%"
+            //self.progressLayer.strokeEnd = CGFloat(progress)
+            self.prgressView.progress = progress
         }
         
     }
